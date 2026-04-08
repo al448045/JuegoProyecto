bf5a81d30105809cd1836f63080a633dd5422a11
rubenstr0y <rubenk17mm@gmail.com>
2026-04-08 14:52:27 +0200

    Wave Scriptable Object Test

---
+++ Assets/ScriptableObjects/Waves/TestWave.cs
@@ -0,0 +1,14 @@
+using NUnit.Framework;
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+[CreateAssetMenu(fileName = "TestWave", menuName = "Scriptable Objects/TestWave")]
+public class TestWave : ScriptableObject
+{
+
+    public List<GameObject> Enemies;
+
+
+
+}
