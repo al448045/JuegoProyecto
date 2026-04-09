0cf3ee0c3d898774ddb0083e0f04eb46340cbf1d
rubenstr0y <rubenk17mm@gmail.com>
2026-04-09 14:42:20 +0200

    waveManager Change

--- Assets/Scripts/General/Waves/Wave.cs
+++ Assets/Scripts/General/Waves/Wave.cs
@@ -5,4 +5,6 @@ using UnityEngine;
 public class Wave : ScriptableObject
 {
     [field: SerializeField] public GameObject[] EnemiesInWave { get; private set; }
+    [field: SerializeField] public WaveManager waveManager;
+
 }
