71038823b957770547cdd46c9ddd9b4d9f1da51d
rubenstr0y <rubenk17mm@gmail.com>
2026-05-19 00:32:26 +0200

    Level + Prefabs

--- Assets/Scripts/General/Auxiliar/CameraFollowPlayer.cs
+++ Assets/Scripts/General/Auxiliar/CameraFollowPlayer.cs
@@ -18,8 +18,8 @@ public class CameraFollowPlayer : MonoBehaviour
         player = GameManager.Instance.player.transform;
         cameraOffset = new Vector3(0, 0, -10);
 
-        maxValue = new Vector3(20, 13, 0);
-        minValue = new Vector3(-20, -13, 0);
+        maxValue = new Vector3(21, 15, 0);
+        minValue = new Vector3(-22, -15, 0);
 
         cameraHeight = camera.orthographicSize;
         cameraWidth = camera.orthographicSize * camera.aspect;
